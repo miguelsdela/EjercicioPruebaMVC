78060ae392a7810b3b44558a5e740b5c54e96383
miguel.sdela <miguel.sdela@babelgroup.com>
2023-03-20 12:20:13 +0100

    Primer commit

---
+++ src/test/java/com/babel/EjercicioRoi/EjercicioRoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babel.EjercicioRoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioRoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
